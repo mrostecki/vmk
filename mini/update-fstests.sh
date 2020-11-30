0a37991d9cbd35cbb93697509f49c259f72d8157
Michal Rostecki <mrostecki@opensuse.org>
2020-11-30 21:32:08 +0100

    mini: update-fstests: Copy fstests sources instead of a tarball
    Copy fstests sources from the given directory or from directory defined
    in a new gitdir_fstests setting.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- mini/update-fstests.sh
+++ mini/update-fstests.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 
-# - copy local tarball of fstests into the VM root
+# - copy local sources of fstests into the VM root
 # - copy the run script
 # - set the run script for autorun
+#
+# usage: $0 [dir]
+
+function die(){ echo "ERROR: $@"; exit 1; }
+
+if [ -z "$1" ]; then
+    if [ -f 'runme-config' ]; then
+	source ./runme-config
+	echo "Update from config path: $gitdir_fstests"
+	dir=$gitdir_fstests
+    else
+	die "no path given and config not found"
+    fi
+else
+    dir="$1"
+    echo "Update from path: $dir"
+fi
+
 
 ./root-mount || { echo "ERROR: no mount"; exit 1; }
 
-echo "Copy fstests tar"
-sudo cp fstests.tar.gz run-fstests.sh mnt
+echo "Rsync fstests sources"
+sudo rsync -vaxAXPH "$dir/" mnt/root/fstests || die "cannot rsync"
+echo "Copy fstests script"
+sudo cp run-fstests.sh mnt
 echo "Set fstests as autorun"
 sudo ln -sf /run-fstests.sh mnt/autorun.sh
 
